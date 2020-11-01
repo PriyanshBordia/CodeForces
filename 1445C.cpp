6fdb143b4a05e4e04445c8bd90fe28fae2d3fab5
PriyanshBordia <priyanshbordia1@gmail.com>
2020-11-01 22:02:00 +0530

    1/11

---
+++ 1445C.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstring>
+#include <math.h>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <map>
+#include <set>
+#include <time.h>
+
+typedef long long ll;
+#define scll(x) scanf("%lld", &x)
+#define pfll(x) printf("%lld\n", x)
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define pb(x) push_back(x)
+
+using namespace std;
+
+void solve()
+{
+	vector<ll> v;	set<ll> st;	map<ll, ll> mp;
+	
+	ll ans(0), sum(0), cnt(0), mx(-1), mn(1000000000);
+	
+	ll p, q; cin >> p >> q;
+
+	if (p % q != 0)
+		ans = p;
+
+	else
+	{	
+		ll j = p / q;
+		
+		while (p % j != 0)
+			j += 1;
+
+		ans = p / j;
+	}
+
+	pfll(ans);
+
+	return;
+}
+
+int main()
+{
+	clock_t start = clock();
+
+	ll t = 1; scll(t);
+	
+	while (t--)
+		solve();
+
+	clock_t end = clock();
+
+	// cout << double(end - start) / CLOCKS_PER_SEC << endl;
+
+	return 0;
+}
