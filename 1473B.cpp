d30afb65712d26d87d8d2c544a69d2e65b4f0326
PriyanshBordia <priyanshbordia1@gmail.com>
2021-02-07 20:57:15 +0530

    7/2

---
+++ 1473B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+typedef long long ll;
+
+using namespace std;
+
+void solve()
+{
+	string s; cin >> s;
+
+	string t; cin >> t;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (1)
+			;
+	}
+
+	return;
+}
+
+int main()
+{
+	ll t; cin >> t;
+
+	while (t--)
+		solve();
+
+	return 0;
+}
